7398c84b4f505f2f5b7b7ef66fb421c893796c3f
madjidimssaoudene1-dotcom <madjidimssaoudene1@gmail.com>
2026-07-13 14:11:56 +0100

    edit

--- src/app/Store.js
+++ src/app/Store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import authReducer from "./slices/authSlice";
 
-export const store = configureStore({
+ const store = configureStore({
   reducer: {
     auth: authReducer,
     theme: themeReducer,
   },
 });
+
+export default store;
